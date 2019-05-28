3fc75a5bea54c422a59abb19c4d2e4bbebb4d3b9
rlatjswlr123 <50874059+rlatjswlr123@users.noreply.github.com>
2019-05-28 20:52:22 +0900

    -

--- practice/mnist.py
+++ practice/mnist.py
@@ -1,4 +1,43 @@
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
+
+# mnist 데이터셋
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# 인풋
+x = tf.placeholder(tf.float32, [None, 784])
+
+# 가중치, 바이어스
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# 답을 넣기 위한 placeholder
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# cross entropy
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+# 학습비율 0.5로 경사하강법 적용하여 크로스 엔트로피 최소화
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# 변수 초기화
+init = tf.global_variables_initializer()
+
+# session에서 모델 실행
 sess = tf.Session()
-print(sess.run(hello))
+
+# 변수 초기화
+sess.run(init)
+
+# 학습 1000번
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# 모델 평가하기
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# 정확도 계산하기
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
---
+++ practice/niftynet_prac.py
@@ -0,0 +1,6 @@
+path_to_search = D:/T1_sample
+filename_contains = MRI
+spatial_window_size = 256, 256, 256
+pixdim = 1.0, 1.0, 1.0
+axcodes = A, R, S 
+interp_order = 3
